fff0f49d8042e0b3181e9c39fe11c0b2f8de751b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-30 20:11:27 +0800

    udpate herE

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let sideA ;
 let sideB ;
 
-document.getElementById("btnside").onclick = function () {
+// document.getElementById("btnside").onclick = function () {
 
-  sideA = document.getElementById("sideA").value ;
-  sideA = Number(sideA) ;
+//   sideA = document.getElementById("sideA").value ;
+//   sideA = Number(sideA) ;
 
-  sideB = document.getElementById("sideB").value ;
-  sideB = Number(sideB) ;
+//   sideB = document.getElementById("sideB").value ;
+//   sideB = Number(sideB) ;
 
-}
+// }
