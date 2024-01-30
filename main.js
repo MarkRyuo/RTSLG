da07d51b60ff7b30e4862b90937efa1c00a17f75
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-30 20:22:51 +0800

    update heRE

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ document.getElementById("textA").innerHTML = sideA ;
 document.getElementById("sideA").value = sideA ;
 
 sideB = Math.ceil(Math.random() * 100) ;
-document.getElementById("textB").innerHTML = sideB
-document.getElementById("sideB").value = sideB
+document.getElementById("textB").innerHTML = sideB ;
+document.getElementById("sideB").value = sideB ;
 
 
 answer = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2)) ;
