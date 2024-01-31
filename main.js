f7904aca62268a926f8e4294ff6d933ffdd3a92c
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-01 07:01:02 +0800

    update herE

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ answer = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2)) ;
 document.getElementById("answer").innerHTML = answer ;
 
 console.log(sideA) ;
+console.log(sideB) ;
+console.log("The c is " + answer) ;
 
 // console.log(window.innerHeight) ;
 // console.log(window.innerWidth) ;
