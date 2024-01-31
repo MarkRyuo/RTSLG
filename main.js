7fef2ea97feb31b1918a446684e0fd59a82d03c4
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-01-31 20:53:54 +0800

    update herE

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 let sideA ;
 let sideB ;
 
-// document.getElementById("btnside").onclick = function () {
-
-//   sideA = document.getElementById("sideA").value ;
-//   sideA = Number(sideA) ;
-
-//   sideB = document.getElementById("sideB").value ;
-//   sideB = Number(sideB) ;
-
-// }
-
 
 sideA = Math.ceil(Math.random() * 100) ;
 document.getElementById("textA").innerHTML = sideA ;
