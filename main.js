7a9955502ba0de764d8b8ad0b374244faaad03ed
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-01 21:20:30 +0800

    update herE:

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ let sideA ;
 let sideB ;
 
 
-sideA = Math.random() * 100 ;
+sideA = Math.random() * -100 + 1 ;
 document.getElementById("textA").innerHTML = sideA ;
 document.getElementById("sideA").value = sideA ;
 
-sideB = Math.random() * 100 ;
+sideB = Math.random() * -100 + 1 ;
 document.getElementById("textB").innerHTML = sideB ;
 document.getElementById("sideB").value = sideB ;
 
